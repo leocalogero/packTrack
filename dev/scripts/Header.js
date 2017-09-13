e68f8ed9def18c054011e18b21a5c9929678e135
Leo Calogero <leo.calogero@gmail.com>
2017-09-13 01:53:48 -0400

    third commit

--- dev/scripts/Header.js
+++ dev/scripts/Header.js
@@ -22,7 +22,7 @@ const Header = (props) => {
 					<div className='wrapper'>
 						<h1>track what you pack</h1>
 						<h2>keep tracking everything you're packing</h2>
-						<p>A simple app to continously track all your packs. Pack Track stores and stamps all the contents of your bags in realtime. Simply add a bag, name it and start tracking your stuff!.</p>
+						<p>A simple app to continously track all your packs. Pack Track&#8482; stores and stamps all the contents of your bags in realtime. Simply add a bag, name it and start tracking your stuff!.</p>
 					</div>
 				</header>
 				</div>
